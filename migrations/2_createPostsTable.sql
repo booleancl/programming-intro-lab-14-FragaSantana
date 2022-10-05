784a6d388e83b9798748612cc500df9854f2fcae
fraga <fragacsz@gmail.com>
2022-10-05 19:40:52 -0300

    Se tabla Posts

--- migrations/2_createPostsTable.sql
+++ migrations/2_createPostsTable.sql
@@ -1,5 +1,10 @@
 CREATE TABLE Posts(
     id INT PRIMARY KEY UNIQUE NOT NULL,
+    Users_id INT,
     title VARCHAR(80),
-    date DATE
-)
+    date DATE,
+CONSTRAINT fk_Users_id
+    FOREIGN KEY(Users_id) 
+	  REFERENCES Users(id)
+	  ON DELETE SET NULL    
+);
