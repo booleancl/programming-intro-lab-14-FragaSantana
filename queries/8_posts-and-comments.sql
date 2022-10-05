51da09bcce02d5efce1afc7fcdbdf3eabcea2591
fraga <fragacsz@gmail.com>
2022-10-05 20:55:07 -0300

    Se realizan consultas ejecicio 8 y 9

--- queries/8_posts-and-comments.sql
+++ queries/8_posts-and-comments.sql
@@ -1,3 +1,4 @@
 /*
 Seleccionar los posts con y sin comentarios
 */
+select us.email,us.id, cm.content  from comments cm full outer join Users us ON us.id = cm.users_id;
--- queries/9_join-between.sql
+++ queries/9_join-between.sql
@@ -2,3 +2,6 @@
 Seleccionar los usuarios que han creado posts entre '2020-05-31' y '2020-07-01'
 */
 
+select ps.users_id, ps.date from users us inner join posts ps
+on us.id = ps.Users_id
+where ps.date between '2020-05-31' and '2020-07-01';
