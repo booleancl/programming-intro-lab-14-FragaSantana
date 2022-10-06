03ffba89399f139d6470b59753bf4aa5bfc7355d
fraga <fragacsz@gmail.com>
2022-10-05 21:55:19 -0300

    se agregan mas querys  ejercicio 8

--- queries/8_posts-and-comments.sql
+++ queries/8_posts-and-comments.sql
@@ -4,3 +4,12 @@ Seleccionar los posts con y sin comentarios
 select ps.id, cm.content from comments cm full outer join posts ps
 ON ps.id = cm.posts_id 
 order by cm.posts_id;
+
+
+SELECT * 
+FROM comments
+RIGHT JOIN posts ON comments.posts_id = posts.id;
+
+SELECT *
+FROM posts
+LEFT JOIN comments ON comments.posts_id = posts.id;
